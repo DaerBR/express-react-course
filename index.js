91d8fa3299d0f2606584eaa4491dae4a3fd11777
Gennadii Guiliakov <gennadii.guliakov@limestonedigital.com>
2026-03-19 14:46:42 +0200

    Init repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello World!'} );
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT)
